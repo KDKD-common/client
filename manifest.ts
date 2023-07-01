81c0813f42676ae452a25ee77e6c6fbd553b9b0c
KDKD-common <kdkd.urur@gmail.com>
2023-07-01 07:10:13 +0000

    chore: vercel test 14

---
+++ manifest.ts
@@ -0,0 +1,13 @@
+const manifest = {
+  metadata: {
+    name: 'React Vite Extension',
+    description: 'React Vite Extension',
+    version: '0.0.1',
+    manifest_version: 3,
+    action: {
+      default_popup: 'extension.html',
+    },
+  },
+};
+
+export default manifest;
---
+++ vite.config.ts
@@ -0,0 +1,52 @@
+import type { PluginOption } from 'vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import * as fs from 'fs';
+import manifest from './manifest';
+
+const outDir = resolve(__dirname, 'dist');
+
+function buildManifest(): PluginOption {
+  function make() {
+    const manifestPath = resolve(outDir, 'manifest.json');
+
+    fs.writeFileSync(manifestPath, JSON.stringify(Object.values(manifest)[0]));
+  }
+
+  return {
+    name: 'make-manifest',
+    generateBundle() {
+      make();
+    },
+  };
+}
+
+function buildExtensionHtml(): PluginOption {
+  function make() {
+    const htmlFilePath = resolve(__dirname, 'extension.html');
+    const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
+    console.log('htmlContent', htmlContent)
+    const htmlPath = resolve(outDir, 'extension.html');
+    fs.writeFileSync(htmlPath, htmlContent);
+  }
+  return {
+    name: 'make-extension-html',
+    generateBundle() {
+      make();
+    },
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), buildManifest(), buildExtensionHtml()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        extension: resolve(__dirname, 'extension.html'),
+      }
+    }
+  }
+});
